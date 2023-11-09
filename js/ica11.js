01fc8084a770e76d6d7f3b8495b75b5ad04e8dbc
ColeMetcalf <come6764@colorado.edu>
2023-11-08 23:43:34 -0700

    ica 11

--- js/ica11.js
+++ js/ica11.js
@@ -1,23 +1,33 @@
-const btn = document.querySelector("button");
-btn.addEventListener("click", displayMessage);
 
-function displayMessage() {
-const body = document.body;
-const panel = document.createElement("div");
-panel.setAttribute("class", "msgBox");
-body.appendChild(panel);
+const btn = document.querySelector('button');
+btn.addEventListener('click', () => displayMessage('Brian: Hi there, how are you today?', 'chat'));
 
-const msg = document.createElement("p");
-msg.textContent = "This is a message box";
-panel.appendChild(msg);
+function displayMessage(msgText,msgType) {
+  const html = document.querySelector('html');
 
-const closeBtn = document.createElement("button");
-closeBtn.textContent = "x";
-panel.appendChild(closeBtn);
+  const panel = document.createElement('div');
+  panel.setAttribute('class','msgBox');
+  html.appendChild(panel);
 
-closeBtn.addEventListener("click", () =>
-  panel.parentNode.removeChild(panel),
-);
-displayMessage();
+  const msg = document.createElement('p');
+  msg.textContent = msgText;
+  panel.appendChild(msg);
 
+  const closeBtn = document.createElement('button');
+  closeBtn.textContent = 'x';
+  panel.appendChild(closeBtn);
+
+  closeBtn.onclick = function() {
+    panel.parentNode.removeChild(panel);
   }
+
+  if(msgType === 'warning') {
+    msg.style.backgroundImage = 'url(icons/warning.png)';
+    panel.style.backgroundColor = 'red';
+  } else if(msgType === 'chat') {
+    msg.style.backgroundImage = 'url(icons/chat.png)';
+    panel.style.backgroundColor = 'aqua';
+  } else {
+    msg.style.paddingLeft = '20px';
+  }
+}
