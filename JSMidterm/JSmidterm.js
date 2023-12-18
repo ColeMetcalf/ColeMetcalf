13b323420d7669a5081a70e753c6e5114f101b2d
ColeMetcalf <come6764@colorado.edu>
2023-12-17 20:31:03 -0700

    Jss midterm

--- JSMidterm/JSmidterm.js
+++ JSMidterm/JSmidterm.js
@@ -1,7 +1,40 @@
 var slider = document.getElementById("myRange");
 var output = document.getElementById("demo");
 
-// Function to show the phone number when the button is clicked
 function showPhoneNumber() {
     output.innerHTML = slider.value;
+}
+function callPhoneNumber() {
+    var valueContainer = document.querySelector(".valuecontainer");
+    var bottomHalf = document.querySelector(".bottom-half");
+
+    var phoneNumber = valueContainer.textContent;
+
+    var existingNumberElement = document.querySelector(".last-entered-number");
+
+    if (existingNumberElement) {
+        existingNumberElement.textContent = "Ring Ring, Calling: " + phoneNumber;
+    } else {
+        var displayElement = document.createElement("div");
+        displayElement.textContent = "Ring Ring, Calling: " + phoneNumber;
+        displayElement.classList.add("last-entered-number");
+
+        bottomHalf.appendChild(displayElement);
+    }
+
+    bottomHalf.style.backgroundColor = "grey";
+}
+function resetApplication() {
+    var bottomHalf = document.querySelector(".bottom-half");
+    var valueContainer = document.querySelector(".valuecontainer");
+    var slider = document.getElementById("myRange");
+
+    var lastEnteredNumberElement = document.querySelector(".last-entered-number");
+    if (lastEnteredNumberElement) {
+        bottomHalf.removeChild(lastEnteredNumberElement);
+    }
+
+    bottomHalf.style.backgroundColor = "";
+
+    valueContainer.textContent = "0";
 }
